e17994c3570a22b1dfb07b8d7e40906f3672add4
Juan Ibarra <jmibarra86@gmail.com>
2023-04-26 18:34:20 -0300

    Implemento y pruebo el middleware

--- scr/router/users.ts
+++ scr/router/users.ts
@@ -1,7 +1,8 @@
 import express, { Router } from "express";
 
 import { getAllUsers } from "../controllers/users";
+import { isAuthenticaded } from "../middlewares";
 
 export default (router: express.Router) => {
-    router.get('/users', getAllUsers);
+    router.get('/users', isAuthenticaded, getAllUsers);
 }

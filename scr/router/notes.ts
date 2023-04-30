0c11d5ffa02d1c1520e3b3df46cee00a12267486
Juan Ibarra <jmibarra86@gmail.com>
2023-04-30 13:55:07 -0300

    Ajustes router

--- scr/router/notes.ts
+++ scr/router/notes.ts
@@ -7,5 +7,4 @@ export default (router: express.Router) => {
     router.get('/notes', isAuthenticated, getAllNotes);
     router.delete('/notes/:id', isAuthenticated, isNoteOwner, deleteNote);
     router.patch('/notes/:id', isAuthenticated, isNoteOwner, updateNote);
-    //Me faltaria un middleware generico para que todos los objetos tengan un owner id y no los puedas editar / borrar si no los tienen.
 }

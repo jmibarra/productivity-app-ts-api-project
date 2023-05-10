ab1853a1192c54f7227908fe81b03cd8e18ebcda
Juan Ibarra <jmibarra86@gmail.com>
2023-05-10 19:31:01 -0300

    Manejo de favoritos en las notas

--- scr/controllers/notes.ts
+++ scr/controllers/notes.ts
@@ -76,6 +76,7 @@ export const updateNote = async (req: express.Request, res: express.Response) =>
       
         const note = req.body.note
       
+        console.log(note)
         if(note){
             note.title = title ? title : note.title;
             note.content = content ? content : note.content;

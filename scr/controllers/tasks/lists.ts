cc15a2369fb916a17a212ce06520effb7d629634
Juan Ibarra <jmibarra86@gmail.com>
2025-01-11 17:41:27 -0300

    se arman los primeros métodos para poder traer las listas y avanzar con el frontend

--- scr/controllers/tasks/lists.ts
+++ scr/controllers/tasks/lists.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { get } from 'lodash';
-import { createList, getNextListOrder } from '../../db/tasks/lists';
+import { createList, getLists, getNextListOrder } from '../../db/tasks/lists';
 
 export const createNewList = async (req: express.Request, res: express.Response) => {
     try{
@@ -24,5 +24,16 @@ export const createNewList = async (req: express.Request, res: express.Response)
     }catch(error){
         console.log(error);
         return res.sendStatus(400)
+    }
+}
+
+export const getAllLists = async (req: express.Request, res: express.Response) => {
+    try {
+        const creator = get(req, 'identity._id') as unknown as string;
+        const lists = await getLists(creator);
+        return res.status(200).json(lists).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
     }
 }
--- scr/router/lists.ts
+++ scr/router/lists.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares";
-import { createNewList } from "../controllers/tasks/lists";
+import { createNewList, getAllLists } from "../controllers/tasks/lists";
 
 export default (router: express.Router) => {
     router.post('/lists', isAuthenticated, createNewList);
+    router.get('/lists', isAuthenticated, getAllLists);
 }
